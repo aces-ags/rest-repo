6d93393800ae91bf999ef9ea75bc8219ddd4dbbb
Agastya Mishra <appleags@Agastyas-MacBook-Pro.local>
2014-08-27 20:08:49 -0500

    added user class

---
+++ src/main/java/com/rest/shopping/order/api/User.java
@@ -0,0 +1,63 @@
+package com.rest.shopping.order.api;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String userName;
+	private String firstName;
+	private String lastName;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
--- src/main/java/com/rest/shopping/order/api/UserRequest.java
+++ src/main/java/com/rest/shopping/order/api/UserRequest.java
@@ -5,53 +5,8 @@ import java.io.Serializable;
 public class UserRequest implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private String email;
-	private String password;
-	private String confirmPassword;
-	private String userName;
-	private String firstName;
-	private String lastName;
+	private User user;
 	private AuthenticationRequest authRequest;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 
 	public AuthenticationRequest getAuthRequest() {
 		return authRequest;
@@ -61,12 +16,12 @@ public class UserRequest implements Serializable {
 		this.authRequest = authRequest;
 	}
 
-	public String getUserName() {
-		return userName;
+	public User getUser() {
+		return user;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 }
